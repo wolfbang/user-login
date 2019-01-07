7a4abd4ee335a928b39c5aa0f573f8b43f38746e
lucas ley <ayaaa168@163.com>
2019-01-07 08:37:28 +0800

    chore:init project

---
+++ src/main/java/com/spacex/user/UserLoginApplication.java
@@ -0,0 +1,11 @@
+package com.spacex.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserLoginApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserLoginApplication.class);
+    }
+}
